3fffdb648e1eac30f68eec8f6c71ac479fdd292a
Jackie Woodburn <jackie.woodburn@softwire.com>
2023-02-14 15:08:26 +0000

    ACADEMY22-409: Initial commit

---
+++ src/main/java/com/whalespottingjava/WhaleSpottingJavaApplication.java
@@ -0,0 +1,14 @@
+package com.whalespottingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleSpottingJavaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WhaleSpottingJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whalespottingjava/WhaleSpottingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whalespottingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleSpottingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
