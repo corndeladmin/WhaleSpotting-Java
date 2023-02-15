49ebe161b15548db63b748c30ed6913124fda190
Jackie Woodburn <jackie.woodburn@softwire.com>
2023-02-15 10:28:43 +0000

    ACADEMY22-409: Added other pages

--- src/main/java/com/whalespottingjava/config/WebMvcConfig.java
+++ src/main/java/com/whalespottingjava/config/WebMvcConfig.java
@@ -1,7 +1,5 @@
 package com.whalespottingjava.config;
 
-import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,13 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/members").setViewName("members");
+        registry.addViewController("/admin").setViewName("admin");
     }
-
-    @Bean
-    public LayoutDialect layoutDialect() {
-        return new LayoutDialect();
-    }
-
 }
