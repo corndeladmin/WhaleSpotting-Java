c43bf9a1475733f76b8621c725b48bc9639174b0
Jackie Woodburn <jackie.woodburn@softwire.com>
2023-02-15 10:03:52 +0000

    ACADEMY22-409: Added nav bar to all pages

--- src/main/java/com/whalespottingjava/config/WebMvcConfig.java
+++ src/main/java/com/whalespottingjava/config/WebMvcConfig.java
@@ -1,17 +1,24 @@
 package com.whalespottingjava.config;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }
