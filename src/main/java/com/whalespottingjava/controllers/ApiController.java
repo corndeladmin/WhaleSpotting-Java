cf91356c45889a5f95f441d2f446d313fe2e2e25
Eyrim <marfxedbot@gmail.com>
2023-10-10 16:15:53 +0100

    Removing test API method, swagger ui works

--- src/main/java/com/whalespottingjava/controllers/ApiController.java
+++ src/main/java/com/whalespottingjava/controllers/ApiController.java
@@ -1,22 +1,7 @@
 package com.whalespottingjava.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-    @GetMapping("/api")
-    public String getApiDocPage() {
-        return "api";
-    }
-
-    @PostMapping("/api")
-    public String postWhale() {
-//        System.out.println(whaleName);
-
-        return "OK";
-    }
 }
