bfcafcfc47f6f420687c3134571b305a92d0cba4
Eyrim <marfxedbot@gmail.com>
2023-10-10 12:13:10 +0100

    New dependencies, swagger config, test endpoint

--- src/main/java/com/whalespottingjava/controllers/ApiController.java
+++ src/main/java/com/whalespottingjava/controllers/ApiController.java
@@ -1,12 +1,23 @@
 package com.whalespottingjava.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ApiController {
     @GetMapping("/api")
     public String getApiDocPage() {
         return "api";
+    }
+
+    @PostMapping("/api")
+    @CrossOrigin(origins = "*")
+    public HttpStatus postWhale(
+            @PathVariable String whaleName
+    ) {
+        System.out.println(whaleName);
+
+        return HttpStatus.OK;
     }
 }
