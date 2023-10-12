56aeb6cc06f4eba3af3d4fe27a53626cc7effe9b
Eyrim <marfxedbot@gmail.com>
2023-10-12 10:53:08 +0100

    Removed unnecessary boxing in Sighting.java

--- src/main/java/com/whalespottingjava/models/database/Sighting.java
+++ src/main/java/com/whalespottingjava/models/database/Sighting.java
@@ -18,13 +18,13 @@ public class Sighting {
     private int member_id;
 
     private Date date;
-    private Double latitude;
-    private Double longitude;
+    private double latitude;
+    private double longitude;
     private Boolean approved;
 
     protected Sighting() {}
     
-    public Sighting(Date date, Double latitude, Double longitude) {
+    public Sighting(Date date, double latitude, double longitude) {
         this.date = date;
         this.latitude = latitude;
         this.longitude = longitude;
@@ -36,19 +36,19 @@ public class Sighting {
         return id;
     }
 
-    public Double getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(Double latitude) {
+    public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
-    public Double getLongitude() {
+    public double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(Double longitude) {
+    public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
