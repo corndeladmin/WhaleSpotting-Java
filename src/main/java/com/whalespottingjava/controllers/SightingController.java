4b23ab9e3115e66700616beb683399e7aa20e678
John Cadfan Williams <cadfanw@gmail.com>
2023-10-12 12:05:41 +0100

    Code Cleanup

--- src/main/java/com/whalespottingjava/controllers/SightingController.java
+++ src/main/java/com/whalespottingjava/controllers/SightingController.java
@@ -1,14 +1,10 @@
 package com.whalespottingjava.controllers;
 
-import com.whalespottingjava.models.database.Sighting;
-import com.whalespottingjava.repositories.SightingRepository;
 import com.whalespottingjava.services.SightingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class SightingController {
@@ -26,13 +22,6 @@ public class SightingController {
         model.addAttribute("sightings", sightingService.getAllSightings());
         return "sighting_test";
     }
-
-
-    //sightings_approved.html is a local file
-    //if you want to use this feature please changed "return "sightings_approved";" to whatever your html file is
-    //Model model is how the data is parsed
-    //The name of the model that is parsed is "approvedSightings" and is filled with "sightingService.getAllApprovedSightings());"
-    //So that in the html file it is parsed to you can use the list of all the approved sightings
     @GetMapping("/sightings/approved")
     public String getAllApprovedSightings(Model model) {
         model.addAttribute("approvedSightings", sightingService.getAllApprovedSightings());
--- src/main/java/com/whalespottingjava/services/SightingService.java
+++ src/main/java/com/whalespottingjava/services/SightingService.java
@@ -1,29 +1,23 @@
 package com.whalespottingjava.services;
-
 import com.whalespottingjava.models.database.Sighting;
 import com.whalespottingjava.repositories.SightingRepository;
-import groovy.transform.stc.ClosureSignatureHint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class SightingService {
-
     private final SightingRepository sightingRepository;
-
     @Autowired
     public SightingService(SightingRepository sightingRepository)
     {
         this.sightingRepository = sightingRepository;
     }
-
     public List<Sighting> getAllSightings()
     {
         return sightingRepository.findAll();
     }
-
-    public List<Sighting> getAllApprovedSightings() { return sightingRepository.findALlApprovedSightings(); }
-
+    public List<Sighting> getAllApprovedSightings()
+    {
+        return sightingRepository.findALlApprovedSightings();
+    }
 }
