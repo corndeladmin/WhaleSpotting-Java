db1a92807541377a430698caffed35fae3cd1628
Aude <72932493+Aude11@users.noreply.github.com>
2023-10-13 10:31:55 +0100

    Update src/main/java/com/whalespottingjava/services/PostCodeService.java
    Co-authored-by: Eyrim <62777325+Eyrim@users.noreply.github.com>

--- src/main/java/com/whalespottingjava/services/PostCodeService.java
+++ src/main/java/com/whalespottingjava/services/PostCodeService.java
@@ -43,6 +43,7 @@ public class PostCodeService {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+        
         return result;
     }
     public double getLatitude(String postCode) {
