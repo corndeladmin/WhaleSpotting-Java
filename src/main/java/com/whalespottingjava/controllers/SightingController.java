736275c69ba6028843b5cb122292408e00308616
Andy Huang <cerberus@ayxh.dev>
2023-10-13 10:31:15 +0000

    Address comments in Sightings controller

--- src/main/java/com/whalespottingjava/controllers/SightingController.java
+++ src/main/java/com/whalespottingjava/controllers/SightingController.java
@@ -2,19 +2,16 @@ package com.whalespottingjava.controllers;
 
 import com.whalespottingjava.models.MemberDetails;
 import com.whalespottingjava.models.database.Sighting;
-import com.whalespottingjava.repositories.SightingRepository;
 import com.whalespottingjava.services.SightingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -46,8 +43,8 @@ public class SightingController {
         return "add_sighting_confirmation";
     }
 
-    @GetMapping("/Sightings")
-    public String getAllSighting(Model model) {
+    @GetMapping("/sightings")
+    public String getAllSightings(Model model) {
         model.addAttribute("sightings", sightingService.getAllSightings());
         return "sighting_test";
     }
