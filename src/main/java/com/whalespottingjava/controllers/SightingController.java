d9d00a8fabc9324be9e700a72df92d08b5e459ee
aniko <budayaniko@outlook.com>
2023-10-13 14:28:59 +0100

    pendingSighting Controller

--- src/main/java/com/whalespottingjava/controllers/SightingController.java
+++ src/main/java/com/whalespottingjava/controllers/SightingController.java
@@ -65,4 +65,11 @@ public class SightingController {
   @GetMapping("/sightings/approved")
   @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
   public void getAllApprovedSightings() {}
+
+  @GetMapping("/sightings/pending")
+  @ResponseStatus(HttpStatus.OK)
+  public String getPendingSightings(Model model) {
+    model.addAttribute("sightings", sightingService.getPendingSightings());
+    return "sighting_test";
+  }
 }
