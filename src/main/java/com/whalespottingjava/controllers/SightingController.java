6c752b5789f3b7da0ff2d228970178f2fa0f4fa9
Andy Huang <cerberus@ayxh.dev>
2023-10-13 12:15:09 +0000

    Fix issues with Sighting and SightingController

--- src/main/java/com/whalespottingjava/controllers/SightingController.java
+++ src/main/java/com/whalespottingjava/controllers/SightingController.java
@@ -4,66 +4,65 @@ import com.whalespottingjava.models.MemberDetails;
 import com.whalespottingjava.models.database.Sighting;
 import com.whalespottingjava.services.SightingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class SightingController {
-    private final SightingService sightingService;
+  private final SightingService sightingService;
 
-    @Autowired
-    public SightingController(SightingService sightingService) {
-        this.sightingService = sightingService;
-    }
+  @Autowired
+  public SightingController(SightingService sightingService) {
+    this.sightingService = sightingService;
+  }
 
-    //renders the add-sighting form
-    @GetMapping("/add-whale-sighting")
-    public String getAddSightingPage(Model model) {
-        model.addAttribute("sighting", new Sighting());
-        return "add_sighting";
-    }
+  // renders the add-sighting form
+  @GetMapping("/add-whale-sighting")
+  public String getAddSightingPage(Model model) {
+    model.addAttribute("sighting", new Sighting());
+    return "add_sighting";
+  }
 
+  @GetMapping("/sightings/test")
+  public String getAllSighting(Model model) {
+    model.addAttribute("sightings", sightingService.getAllSightings());
+    return "sightings_map";
+  }
 
-   @GetMapping("/sightings/test")
-   public String getAllSighting(Model model) {
-       model.addAttribute("sightings", sightingService.getAllSightings());
-       return "sightings_map";
-   }
-//
-//    @GetMapping("/sightings/approved")
-//    public String getAllApprovedSightings(Model model) {
-//        model.addAttribute("approvedSightings", this.sightingService.getAllApprovedSightings());
-//        return "sightings_approved";
-//    }
+  //
+  //    @GetMapping("/sightings/approved")
+  //    public String getAllApprovedSightings(Model model) {
+  //        model.addAttribute("approvedSightings", this.sightingService.getAllApprovedSightings());
+  //        return "sightings_approved";
+  //    }
 
-    //submits the add-sighting form data
-    @PostMapping("/add-whale-sighting")
-    public String submitSighting(@ModelAttribute Sighting sighting, Model model) {
-        model.addAttribute("sighting", sighting);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication authentication = securityContext.getAuthentication();
-        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
-        sighting.setMemberId(memberDetails.getMember().getId());
-        sightingService.addSighting(sighting);
-        return "add_sighting_confirmation";
-    }
+  // submits the add-sighting form data
+  @PostMapping("/add-whale-sighting")
+  public String submitSighting(@ModelAttribute Sighting sighting, Model model) {
+    model.addAttribute("sighting", sighting);
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+    Authentication authentication = securityContext.getAuthentication();
+    MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+    sighting.setMemberId(memberDetails.getMember().getId());
+    sightingService.addSighting(sighting);
+    return "add_sighting_confirmation";
+  }
 
+  @GetMapping("/sightings")
+  public String getAllSightings(Model model) {
+    model.addAttribute("sightings", sightingService.getAllSightings());
+    return "sighting_test";
+  }
 
-    @GetMapping("/sightings")
-    public String getAllSightings(Model model) {
-        model.addAttribute("sightings", sightingService.getAllSightings());
-        return "sighting_test";
-    }
-
-    @GetMapping("/sightings/approved")
-    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
-    public void getAllApprovedSightings() {
-    }
+  @GetMapping("/sightings/approved")
+  @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+  public void getAllApprovedSightings() {}
 }
