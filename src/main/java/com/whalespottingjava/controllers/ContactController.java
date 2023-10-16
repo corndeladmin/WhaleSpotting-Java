66af8a6e1876d167eefffc109e0b89b94151e730
andrejz2010 <zacharovandrej1@gmail.com>
2023-10-16 14:48:12 +0100

    added contact form

---
+++ src/main/java/com/whalespottingjava/controllers/ContactController.java
@@ -0,0 +1,12 @@
+package com.whalespottingjava.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+    @GetMapping("/contact")
+    public String showContactForm() {
+        return "contact_form";
+    }
+}
