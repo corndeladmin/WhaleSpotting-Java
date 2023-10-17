c4affe7a7bb98e531cc6056eea2ea425c0272e17
aniko <budayaniko@outlook.com>
2023-10-17 12:29:59 +0100

    bug with auth

--- src/main/java/com/whalespottingjava/controllers/HomeController.java
+++ src/main/java/com/whalespottingjava/controllers/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.whalespottingjava.models.MemberDetails;
+import com.whalespottingjava.models.enums.MemberRole;
 
 @Controller
 public class HomeController {
@@ -17,6 +18,12 @@ public class HomeController {
         Authentication authentication = securityContext.getAuthentication();
         Boolean isLoggedIn = authentication.isAuthenticated();
         model.addAttribute("isLoggedIn", isLoggedIn);
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+        //use ternary operator
+        MemberRole role = memberDetails.getMember().getRole();
+        if (role == MemberRole.ADMIN) {
+            model.addAttribute("role", role );
+        }
         return "home";
     }
 }
