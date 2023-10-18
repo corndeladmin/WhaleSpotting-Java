f5396169f02d03c6ee12f432807b57b8f659fb9c
CharlesMarcusSmith <charliemarcussmith@googlemail.com>
2023-10-18 15:25:06 +0100

    CS: Draft back end, does nothing?.

--- src/main/java/com/whalespottingjava/controllers/MembersController.java
+++ src/main/java/com/whalespottingjava/controllers/MembersController.java
@@ -1,7 +1,17 @@
 package com.whalespottingjava.controllers;
 
+import com.whalespottingjava.models.MemberDetails;
+import com.whalespottingjava.models.database.Member;
+import com.whalespottingjava.models.database.Sighting;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class MembersController {
@@ -12,6 +22,17 @@ public class MembersController {
 
     @GetMapping("/members/myaccount")
     public String getMyAccountPage() {
+        return "my_account";
+    }
+
+    @PostMapping("/change-password")
+    public String submitSighting(@ModelAttribute Member member, Model model, String oldPassword, String newPassword) {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+        if(memberDetails.getMember().getPassword().equals(oldPassword)){ //encrypted or normal?
+            memberDetails.getMember().setPassword(newPassword);
+        }
         return "my_account";
     }
 }
