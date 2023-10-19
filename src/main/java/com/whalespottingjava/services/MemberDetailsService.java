35f9837f8e099dc78a150cba52eb2bca9a46d527
Eyrim <marfxedbot@gmail.com>
2023-10-19 10:49:32 +0100

    Added member details population

--- src/main/java/com/whalespottingjava/services/MemberDetailsService.java
+++ src/main/java/com/whalespottingjava/services/MemberDetailsService.java
@@ -3,11 +3,14 @@ package com.whalespottingjava.services;
 import com.whalespottingjava.models.MemberDetails;
 import com.whalespottingjava.models.database.Member;
 import com.whalespottingjava.repositories.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MemberDetailsService implements UserDetailsService {
@@ -27,5 +30,13 @@ public class MemberDetailsService implements UserDetailsService {
         }
 
         return new MemberDetails(member);
+    }
+
+    public Member loadMemberById(long id) {
+        return memberRepository
+                .findById(id)
+                .orElseThrow(
+                        EntityNotFoundException::new
+                );
     }
 }
